302fe9e9efbf8c3f780265b3e17c1c42e55fbf8d
Danielle Sweeny <dsweeny1@gmail.com>
2022-09-21 09:32:19 -0600

    Add test files

---
+++ test/Destination-test.js
@@ -0,0 +1,78 @@
+import { expect } from "chai";
+import Destination from "../src/classes/Destination";
+
+describe("Destination", () => {
+    let destination1;
+    let destination2;
+    let destinations;
+
+    beforeEach(() => {
+        destination1 = new Destination(1)
+        destination2 = new Destination(2)
+        destinations = [
+            {id: 1, 
+            destination: 'Lima, Peru', 
+            estimatedLodgingCostPerDay: 70,
+            estimatedFlightCostPerPerson: 400,
+            image: 'https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80',
+            alt: 'overview of city buildings with a clear sky'},
+            {id: 2, 
+                destination: 'Stockholm, Sweden', 
+                estimatedLodgingCostPerDay: 100,
+                estimatedFlightCostPerPerson: 780,
+                image: 'https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+                alt: 'city with boats on the water during the day time'}
+        ]
+    })
+
+    it('Should be a function', () => {
+        expect(Destination).to.be.a('function')
+    })
+
+    it('Should be able to get the destination name by id number', () => {
+        destination1.getDestinationName(destinations)
+        destination2.getDestinationName(destinations)
+        expect(destination1.destination).to.equal('Lima, Peru')
+        expect(destination2.destination).to.equal('Stockholm, Sweden')
+    })
+
+    it('Should be able to get the estimated lodging cost per day by id number', () => {
+        destination1.getDestinationLodgingCost(destinations)
+        // console.log(destination1)
+        destination2.getDestinationLodgingCost(destinations)
+        expect(destination1.estimatedLodgingCost).to.equal(70)
+        expect(destination2.estimatedLodgingCost).to.equal(100)
+    })
+
+    it('Should be able to get the estimated flight cost per person by id number', () => {
+        destination1.getDestinationFlightCost(destinations)
+        // console.log(destination1)
+        destination2.getDestinationFlightCost(destinations)
+        expect(destination1.estimatedFlightCost).to.equal(400)
+        expect(destination2.estimatedFlightCost).to.equal(780)
+    })
+
+    it('Should be able to get the estimated flight cost per person by id number', () => {
+        destination1.getDestinationTotalCost(destinations)
+        // console.log(destination1)
+        destination2.getDestinationTotalCost(destinations)
+        expect(destination1.totalDestinationCost).to.equal(517)
+        expect(destination2.totalDestinationCost).to.equal(968)
+    })
+
+    it('Should be able to get the destination image by id number', () => {
+        destination1.getDestinationImage(destinations)
+        // console.log(destination1)
+        destination2.getDestinationImage(destinations)
+        expect(destination1.image).to.equal('https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80')
+        expect(destination2.image).to.equal('https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80')
+    })
+
+    it('Should be able to get the destination alt text by id number', () => {
+        destination1.getDestinationAltText(destinations)
+        // console.log(destination1)
+        destination2.getDestinationAltText(destinations)
+        expect(destination1.alt).to.equal('overview of city buildings with a clear sky')
+        expect(destination2.alt).to.equal('city with boats on the water during the day time')
+    })
+})
---
+++ test/Session-test.js
@@ -0,0 +1,120 @@
+import { expect } from "chai";
+import Session from "../src/classes/Session";
+import Trip from "../src/classes/Trip";
+import Traveler from "../src/classes/Traveler";
+import Destination from "../src/classes/Destination";
+
+describe('Session', () => {
+let trip1;
+let trip2;
+let trip3;
+let trip4;
+let trips;
+let traveler1;
+let traveler2;
+let travelers;
+let destination1;
+let destination2;
+let destination3;
+let destination4;
+let destinations;
+let session;
+
+beforeEach(() => {
+    trip1 = new Trip(1, 1, 1, 1, "2022/09/16", 8, "approved", [])
+    trip2 = new Trip(2, 2, 2, 5, "2022/10/04", 18, "approved", [])
+    trip3 = new Trip(3, 1, 3, 3, "2022/05/22", 17, "approved", [])
+    trip4 = new Trip(4, 1, 4, 2, "2022/02/25", 10, "pending", [])
+
+    traveler1 = new Traveler({id: 1, name: 'Ham Leadbeater', travelerType: 'relaxer'})
+    // console.log('traveler1', traveler1)
+    traveler2 = new Traveler({id: 2, name: 'Rachael Vaughten', travelerType: 'thrill-seeker'})
+
+    destination1 = new Destination(1, 'Lima, Peru', 70, 400, 517, 'https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80', 'overview of city buildings with a clear sky')
+    // console.log(destination1)
+    destination2 = new Destination(2, 'Stockholm, Sweden', 100, 780, 968, 'https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80', 'city with boats on the water during the day time')
+    destination3 = new Destination(3, 'Sydney, Austrailia', 130, 950, 1188, 'https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80', 'opera house and city buildings on the water with boats')
+    destination4 = new Destination(4, 'Cartagena, Colombia', 65, 350, 902, 'https://images.unsplash.com/photo-1558029697-a7ed1a4b94c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80', 'boats at a dock during the day time')
+
+    trips = [trip1, trip2, trip3, trip4]
+    travelers = [traveler1, traveler2]
+    destinations = [destination1, destination2, destination3, destination4]
+
+    session = new Session(travelers, destinations, trips)
+    })
+
+    it('Should be a function', () => {
+        expect(Session).to.be.a('function')
+    })
+
+    it('Should be able to show a traveler all of their trips', () => {
+        // console.log(destination1)
+        expect(session.findAllTripsByTraveler(1)).to.deep.equal([
+            {
+              alt: 'overview of city buildings with a clear sky',
+              date: '2022/09/16',
+              destination: 'Lima, Peru',
+              duration: 8,
+              id: 1,
+              image: 'https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80',
+              status: 'approved',
+              userID: 1,
+              travelers: 1,
+              suggestedActivities: [],
+              totalTripCost: 517,
+              withAgentFee: 568.7
+            },
+            {
+              id: 3,
+              userID: 1,
+              travelers: 3,
+              withAgentFee: 3564,
+              date: '2022/05/22',
+              duration: 17,
+              status: 'approved',
+              suggestedActivities: [],
+              destination: 'Sydney, Austrailia',
+              totalTripCost: 1188,
+              image: 'https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+              alt: 'opera house and city buildings on the water with boats'
+            },
+            {
+              id: 4,
+              userID: 1,
+              travelers: 2,
+              date: '2022/02/25',
+              duration: 10,
+              status: 'pending',
+              suggestedActivities: [],
+              destination: 'Cartagena, Colombia',
+              withAgentFee: 1804,
+              totalTripCost: 902,
+              image: 'https://images.unsplash.com/photo-1558029697-a7ed1a4b94c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+              alt: 'boats at a dock during the day time'
+            }
+          ])
+    })
+
+    it('Should return the traverlers pending trips', () => {
+      session.findAllTripsByTraveler(1)
+        expect(session.getPendingTrips()).to.deep.equal([{
+            id: 4,
+            userID: 1,
+            travelers: 2,
+            date: '2022/02/25',
+            duration: 10,
+            status: 'pending',
+            suggestedActivities: [],
+            destination: 'Cartagena, Colombia',
+            totalTripCost: 902,
+            withAgentFee: 1804,
+            image: 'https://images.unsplash.com/photo-1558029697-a7ed1a4b94c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+            alt: 'boats at a dock during the day time'
+          }])
+    })
+
+    it('Should return the total trip cost', () => {
+      session.findAllTripsByTraveler(1)
+        expect(session.getTotalSpent(1)).to.equal(5885)
+    })
+})
