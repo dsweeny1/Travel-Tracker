b7b2ae8d1f37ec4e90a508a022e2a77023a2ac88
Danielle Sweeny <dsweeny1@gmail.com>
2022-09-25 11:59:10 -0600

    Update Session class, update tests

--- test/Destination-test.js
+++ test/Destination-test.js
@@ -33,50 +33,37 @@ describe("Destination", () => {
         expect(Destination).to.be.a('function')
     })
 
-    it('Should be able to get the destination name by id number', () => {
-        destination1.getDestinationName(destinations)
-        destination2.getDestinationName(destinations)
-        expect(destination1.destination).to.equal('Lima, Peru')
-        expect(destination2.destination).to.equal('Stockholm, Sweden')
+    it('Should be an instance of Destination', () => {
+        expect(destination1).to.be.an.instanceOf(Destination)
     })
 
-    it('Should be able to get the estimated lodging cost per day by id number', () => {
-        destination1.getDestinationLodgingCost(destinations)
-        // console.log(destination1)
-        destination2.getDestinationLodgingCost(destinations)
-        expect(destination1.estimatedLodgingCost).to.equal(70)
-        expect(destination2.estimatedLodgingCost).to.equal(100)
-    })
+    it("Should have an id", () => {
+        expect(destinations[0].id).to.equal(1);
+        expect(destinations[1].id).to.equal(2);
+    });
 
-    it('Should be able to get the estimated flight cost per person by id number', () => {
-        destination1.getDestinationFlightCost(destinations)
-        // console.log(destination1)
-        destination2.getDestinationFlightCost(destinations)
-        expect(destination1.estimatedFlightCost).to.equal(400)
-        expect(destination2.estimatedFlightCost).to.equal(780)
-    })
+    it("Should have a destination", () => {
+        expect(destinations[0].destination).to.equal('Lima, Peru');
+        expect(destinations[1].destination).to.equal('Stockholm, Sweden');
+    });
 
-    it('Should be able to get the estimated flight cost per person by id number', () => {
-        destination1.getDestinationTotalCost(destinations)
-        // console.log(destination1)
-        destination2.getDestinationTotalCost(destinations)
-        expect(destination1.totalDestinationCost).to.equal(517)
-        expect(destination2.totalDestinationCost).to.equal(968)
-    })
+    it("Should have an estimated lodging cost per day", () => {
+        expect(destinations[0].estimatedLodgingCostPerDay).to.equal(70);
+        expect(destinations[1].estimatedLodgingCostPerDay).to.equal(100);
+    });
 
-    it('Should be able to get the destination image by id number', () => {
-        destination1.getDestinationImage(destinations)
-        // console.log(destination1)
-        destination2.getDestinationImage(destinations)
-        expect(destination1.image).to.equal('https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80')
-        expect(destination2.image).to.equal('https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80')
-    })
+    it("Should have an estimated flight cost per person", () => {
+        expect(destinations[0].estimatedFlightCostPerPerson).to.equal(400);
+        expect(destinations[1].estimatedFlightCostPerPerson).to.equal(780);
+    });
 
-    it('Should be able to get the destination alt text by id number', () => {
-        destination1.getDestinationAltText(destinations)
-        // console.log(destination1)
-        destination2.getDestinationAltText(destinations)
-        expect(destination1.alt).to.equal('overview of city buildings with a clear sky')
-        expect(destination2.alt).to.equal('city with boats on the water during the day time')
-    })
+    it("Should have an image", () => {
+        expect(destinations[0].image).to.equal('https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80');
+        expect(destinations[1].image).to.equal('https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');
+    });
+
+    it("Should have alt text for the image", () => {
+        expect(destinations[0].alt).to.equal('overview of city buildings with a clear sky');
+        expect(destinations[1].alt).to.equal('city with boats on the water during the day time');
+    });
 })
