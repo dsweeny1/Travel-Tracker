3337d71a9711cf053c79e5399fa5f61381fda414
Danielle Sweeny <dsweeny1@gmail.com>
2022-09-26 14:01:05 -0600

    Final commit

--- src/classes/Destination.js
+++ src/classes/Destination.js
@@ -3,7 +3,7 @@ export default class Destination {
         this.id = destination.id,
         this.destination = destination.destination
         this.estimatedLodgingCostPerDay = destination.estimatedLodgingCostPerDay,
-        this.estimatedFlightCost = destination.estimatedFlightCostPerPerson,
+        this.estimatedFlightCostPerPerson = destination.estimatedFlightCostPerPerson,
         this.image = destination.image,
         this.alt = destination.alt
     }
--- test/Session-test.js
+++ test/Session-test.js
@@ -21,19 +21,83 @@ let destinations;
 let session;
 
 beforeEach(() => {
-    trip1 = new Trip(1, 1, 1, 1, "2022/09/16", 8, "approved", [])
-    trip2 = new Trip(2, 2, 2, 5, "2022/10/04", 18, "approved", [])
-    trip3 = new Trip(3, 1, 3, 3, "2022/05/22", 17, "approved", [])
-    trip4 = new Trip(4, 1, 4, 2, "2022/02/25", 10, "pending", [])
+    trip1 = new Trip({
+      id: 1, 
+      userID: 1, 
+      destinationID: 1, 
+      travelers: 1, 
+      date: "2022/09/16", 
+      duration: 8, 
+      status: "approved", 
+      suggestedActivities: []
+    })
+    trip2 = new Trip({
+      id: 2, 
+      userID: 2, 
+      destinationID: 2, 
+      travelers: 5, 
+      date: "2022/10/04", 
+      duration: 18, 
+      status: "approved", 
+      suggestedActivities: []
+    })
+    trip3 = new Trip({
+      id: 3, 
+      userID: 1, 
+      destinationID: 3, 
+      travelers: 3, 
+      date: "2022/05/22", 
+      duration: 17, 
+      status: "approved", 
+      suggestedActivities: []
+    })
+    trip4 = new Trip({
+      id: 4, 
+      userID: 1, 
+      destinationID: 4, 
+      travelers: 2, 
+      date: "2022/02/25", 
+      duration: 10, 
+      status: "pending", 
+      suggestedActivities: []
+    })
 
     traveler1 = new Traveler({id: 1, name: 'Ham Leadbeater', travelerType: 'relaxer'})
     traveler2 = new Traveler({id: 2, name: 'Rachael Vaughten', travelerType: 'thrill-seeker'})
 
-    destination1 = new Destination(1, 'Lima, Peru', 70, 400, 517, 'https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80', 'overview of city buildings with a clear sky')
+    destination1 = new Destination({
+      id: 1, 
+      destination: 'Lima, Peru', 
+      estimatedLodgingCostPerDay: 70, 
+      estimatedFlightCostPerPerson: 400, 
+      image: 'https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80', 
+      alt: 'overview of city buildings with a clear sky'
+    })
   
-    destination2 = new Destination(2, 'Stockholm, Sweden', 100, 780, 968, 'https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80', 'city with boats on the water during the day time')
-    destination3 = new Destination(3, 'Sydney, Austrailia', 130, 950, 1188, 'https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80', 'opera house and city buildings on the water with boats')
-    destination4 = new Destination(4, 'Cartagena, Colombia', 65, 350, 902, 'https://images.unsplash.com/photo-1558029697-a7ed1a4b94c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80', 'boats at a dock during the day time')
+    destination2 = new Destination({
+      id: 2, 
+      destination: 'Stockholm, Sweden', 
+      estimatedLodgingCostPerDay: 100, 
+      estimatedFlightCostPerPerson: 780, 
+      image: 'https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80', 
+      alt: 'city with boats on the water during the day time'
+    })
+    destination3 = new Destination({
+      id: 3, 
+      destination: 'Sydney, Austrailia', 
+      estimatedLodgingCostPerDay: 130, 
+      estimatedFlightCostPerPerson: 950, 
+      image: 'https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80', 
+      alt: 'opera house and city buildings on the water with boats'
+    })
+    destination4 = new Destination({
+      id: 4, 
+      destination: 'Cartagena, Colombia', 
+      estimatedLodgingCostPerDay: 65, 
+      estimatedFlightCostPerPerson: 350, 
+      image: 'https://images.unsplash.com/photo-1558029697-a7ed1a4b94c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80', 
+      alt: 'boats at a dock during the day time'
+    })
 
     trips = [trip1, trip2, trip3, trip4]
     travelers = [traveler1, traveler2]
@@ -47,12 +111,15 @@ beforeEach(() => {
     })
 
     it('Should be able to show a traveler all of their trips', () => {
-        session.findAllTripsByTraveler(traveler1.id)
+      console.log(session)
+      expect(session.eachTravelerTrips).to.deep.equal([])
+        session.findAllTripsByTraveler(1)
         expect(session.eachTravelerTrips).to.deep.equal([
             {
               alt: 'overview of city buildings with a clear sky',
               date: '2022/09/16',
               destination: 'Lima, Peru',
+              destinationID: 1,
               duration: 8,
               id: 1,
               image: 'https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80',
@@ -60,20 +127,21 @@ beforeEach(() => {
               userID: 1,
               travelers: 1,
               suggestedActivities: [],
-              totalTripCost: 517,
-              withAgentFee: 568.7
+              totalTripCost: 960,
+              withAgentFee: 1056
             },
             {
               id: 3,
               userID: 1,
+              destinationID: 3,
               travelers: 3,
-              withAgentFee: 3564,
+              withAgentFee: 5566,
               date: '2022/05/22',
               duration: 17,
               status: 'approved',
               suggestedActivities: [],
               destination: 'Sydney, Austrailia',
-              totalTripCost: 1188,
+              totalTripCost: 5060,
               image: 'https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
               alt: 'opera house and city buildings on the water with boats'
             },
@@ -82,20 +150,40 @@ beforeEach(() => {
               userID: 1,
               travelers: 2,
               date: '2022/02/25',
+              destinationID: 4,
               duration: 10,
               status: 'pending',
               suggestedActivities: [],
               destination: 'Cartagena, Colombia',
-              withAgentFee: 1804,
-              totalTripCost: 902,
+              withAgentFee: 1485,
+              totalTripCost: 1350,
               image: 'https://images.unsplash.com/photo-1558029697-a7ed1a4b94c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
               alt: 'boats at a dock during the day time'
             }
           ])
     })
 
-    it.only('Should return the total trip cost', () => {
+    it('Should return the traverlers pending trips', () => {
       session.findAllTripsByTraveler(1)
-        expect(session.getTotalSpent('2022')).to.equal(5885)
+        expect(session.getPendingTrips()).to.deep.equal([{
+            id: 4,
+            userID: 1,
+            travelers: 2,
+            date: '2022/02/25',
+            destinationID: 4,
+            duration: 10,
+            status: 'pending',
+            suggestedActivities: [],
+            destination: 'Cartagena, Colombia',
+            totalTripCost: 1350,
+            withAgentFee: 1485,
+            image: 'https://images.unsplash.com/photo-1558029697-a7ed1a4b94c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+            alt: 'boats at a dock during the day time'
+          }])
+    })
+
+    it('Should return the total trip cost', () => {
+      session.findAllTripsByTraveler(1)
+        expect(session.getTotalSpent('2022')).to.equal('8107.00')
     })
 })
